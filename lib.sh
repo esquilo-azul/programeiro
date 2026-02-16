1f679e115ed45387ca75adae3c4f71019e59754a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-16 10:18:07 -0300

    _p_find_program_in_path(): extract from p_find_program().

--- lib.sh
+++ lib.sh
@@ -38,16 +38,32 @@ function p_paths {
 function p_find_program {
   local prgname=$1
   p_paths | while read path; do
-    local prgpath="$path""$prgname"
-    local prgbasename="$(basename "$prgpath")"
-    find "$path" -path "$prgpath*" | while read line; do
-      filename=$(basename "$line")
-      filename="${filename%.*}"
-      if [ "$filename" == "$prgbasename" ]; then
-        echo "$line"
-      fi
-    done
+    prgpath="$(_p_find_program_in_path "$prgname" "$path")"
+    if [[ -n "$prgpath" ]]; then
+      printf '%s\n' "${prgpath}"
+      return
+    fi
   done
+
+  return $PROGRAMEIRO_NOT_FOUND_ERROR
+}
+
+function _p_find_program_in_path {
+  local prgname=$1
+  local path=$2
+  local prgpath="$path""$prgname"
+  local prgbasename="$(basename "$prgpath")"
+
+  find "$path" -path "$prgpath*" | while read line; do
+    filename=$(basename "$line")
+    filename="${filename%.*}"
+    if [ "$filename" == "$prgbasename" ]; then
+      echo "$line"
+      return
+    fi
+  done
+
+  return $PROGRAMEIRO_NOT_FOUND_ERROR
 }
 
 function p_run {
